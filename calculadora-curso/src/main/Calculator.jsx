70906a0b9505fe9e9e96c39f8dab3b33cdb9e0bb
Feitoza, Diego <diegofeitoza@yahoo.com.br>
2019-09-09 17:13:51 -0300

    Tentando subir para o git pela milesima vez

---
+++ calculadora-curso/src/main/Calculator.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import './Calculator.css'
+
+export default class Calculator extends Component{
+    render(){
+        return(
+            <div>
+                <h1>Calculator</h1>
+                <div className="calculator">
+
+                </div>
+            </div>
+        )
+    }
+}
