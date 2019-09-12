981e97715f795dda9ce13d448adcba4050499661
Feitoza, Diego <diegofeitoza@yahoo.com.br>
2019-09-12 11:32:50 -0300

    Att aulas para o novo curso

--- calculadora-curso/src/main/Calculator.jsx
+++ calculadora-curso/src/main/Calculator.jsx
@@ -3,7 +3,18 @@ import './Calculator.css'
 import Display from '../components/Display'
 import Button from '../components/Button'
 
+const initialState = {
+    displayValue: '0',
+    clearDisplay: false,
+    operation: null,
+    values: [0,0],
+    current: 0
+}
+
 export default class Calculator extends Component{
+
+    state = {...initialState}
+
     constructor(props){
         super(props)
         this.clearMemory = this.clearMemory.bind(this)
@@ -11,12 +22,8 @@ export default class Calculator extends Component{
         this.addDigit = this.addDigit.bind(this)
     }
 
-    state = {
-        teste: 'teste'
-    }
-
     clearMemory() {
-        console.log('Limpar')
+        this.setState({...initialState})
     }
 
     setOperation(operation) {
@@ -24,7 +31,28 @@ export default class Calculator extends Component{
     }
 
     addDigit(n){
-        console.log(n)
+        //Validação de 1 ponto no numero, não autoriza 2 pontos
+        if(n === '.' && this.state.displayValue.includes('.')){
+            return
+        }
+
+        // Se o primeiro numero for 0, o usuario n pode digitar 0 várias vezes
+        const clearDisplay = this.state.displayValue === '0' 
+                            || this.state.clearDisplay
+        //Verifica 0 tbm
+        const currentValue = clearDisplay ? '' : this.state.displayValue
+        //Salva o valor atual do display
+        const displayValue = currentValue + n
+
+        //Salva o numero dentro do estado        
+        this.setState({displayValue, clearDisplay: false})
+
+        if(n !== '.'){
+            const i = this.state.current
+            const newValue = parseFloat(displayValue)
+            const values = [...this.state.values]
+            this.setState({})
+        }
     }
 
     render(){
@@ -32,7 +60,7 @@ export default class Calculator extends Component{
             <div>
                 <h1>Calculator</h1>
                 <div className="calculator">
-                    <Display value={100}/>
+                    <Display value={this.state.displayValue}/>
 
                     <Button label="AC" click={this.clearMemory} triple/>
 
